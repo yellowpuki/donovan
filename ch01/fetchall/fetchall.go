c6ba0da4f39229896c6185a754cc4d7f4c1547d8
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-04-02 21:03:14 +0300

    add fetchall: parralel fetch URLs

---
+++ ch01/fetchall/fetchall.go
@@ -0,0 +1,44 @@
+// fetchall выполняет параллельную выборку URL и сообщает
+// о затраченном времени и размере ответа для каждого из них.
+package main
+
+import (
+	"fmt"
+	"io"
+	"net/http"
+	"os"
+	"time"
+)
+
+func main() {
+	start := time.Now()
+	ch := make(chan string)
+
+	for _, url := range os.Args[1:] {
+		go fetch(url, ch)
+	}
+
+	for range os.Args[1:] {
+		fmt.Println(<-ch)
+	}
+	fmt.Printf("%0.0fs elapsed\n", time.Since(start).Seconds())
+}
+
+func fetch(url string, ch chan<- string) {
+	start := time.Now()
+	resp, err := http.Get(url)
+	if err != nil {
+		ch <- fmt.Sprint(err)
+		return
+	}
+
+	nbytes, err := io.Copy(io.Discard, resp.Body)
+	resp.Body.Close()
+	if err != nil {
+		ch <- fmt.Sprintf("while reading %s: %v", url, err)
+		return
+	}
+
+	secs := time.Since(start).Seconds()
+	ch <- fmt.Sprintf("%0.0fs %7d %s", secs, nbytes, url)
+}
