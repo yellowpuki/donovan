efeac75183d1e746b4ff71845c1091ab69b16d47
Pavel Timokhovich <yellowpukipost@gmail.com>
2023-04-02 21:49:39 +0300

    add server2: simple echo server with request counter

---
+++ ch01/server2/server2.go
@@ -0,0 +1,33 @@
+// Server2 - минимальный "echo" - сервер со счетчиком запросов.
+package main
+
+import (
+	"fmt"
+	"log"
+	"net/http"
+	"sync"
+)
+
+var (
+	mu    sync.Mutex
+	count int
+)
+
+func main() {
+	http.HandleFunc("/", handler)
+	http.HandleFunc("/count", counter)
+	log.Fatal(http.ListenAndServe("localhost:8000", nil))
+}
+
+func handler(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	count++
+	mu.Unlock()
+	fmt.Fprintf(w, "URL.Path = %q\n", r.URL.Path)
+}
+
+func counter(w http.ResponseWriter, r *http.Request) {
+	mu.Lock()
+	fmt.Fprintf(w, "counter: %d", count)
+	mu.Unlock()
+}
